05ae0505c8eb2b6fe109059258486d410b2bea19
Pascal Jedicke <pascal.jedicke@icloud.com>
2026-02-13 19:58:10 +0100

    feat(app): add Picture type

---
+++ packages/pixels-app/src/types/types.pixels.ts
@@ -0,0 +1,14 @@
+export interface Picture {
+    id: number,
+    userId: number,
+    url: string,
+    title: string,
+    descr: string,
+    createdAt: string,
+    location: string,
+    cameraModel: string,
+    aperture: string,
+    iso: number,
+    focalLength: string,
+    shutterSpeed: string,
+}
